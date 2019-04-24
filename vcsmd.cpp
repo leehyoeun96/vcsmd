9eb59e555e8a3dc41cd45c0dbaeed3c840a87f41
leehyoeun96 <dlgydms412@gmail.com>
2019-04-24 16:28:32 +0900

    test

--- vcsmd.cpp
+++ vcsmd.cpp
@@ -20,35 +20,6 @@
 #define LOCKMODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
 sigset_t mask;
 
-/*
-   static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
-{
-    int errno_save, n;
-    char buf[MAXLINE];
-
-    errno_save = errno;
-#ifdef HAVE_VSNPRINTF
-    vsnprintf(buf, sizeof(buf), fmt, ap);
-#else
-    vsprintf(buf, fmt, ap);
-#endif
-    n = strlen(buf);
-    if(errnoflag)
-        snprintf(buf+n, sizeof(buf)-n, ": %s", sterror(errno_save));
-    strcat(buf, "\n");
-    if(daemon_proc){
-    syslog(level, buf);
-    }
-    else{
-        fflush(stdout);
-        fputs(buf, stderr);
-        fflush(stderr);
-    }
-    return;
-}
-*/
-
-
 int lockfile(int fd)
 {
     struct flock fl;
