4b7ad7569445d58ab1c688302942df7d9ca4c80c
leehyoeun96 <dlgydms412@gmail.com>
2019-05-20 14:05:13 +0900

    delete_mm

--- control_server_mm/killvcsmd.sh
+++
@@ -1,8 +0,0 @@
-
-echo -e "\n"
-ps -axj | grep -E 'vcsmd|VCF'
-echo -e "\n"
-killall -9 vcsmd.exe VCFserver_pc.exe VCFserver_rpi.exe
-echo -e "\n"
-ps -axj | grep -E 'vcsmd|VCF'
-echo -e "\n"
--- control_server_mm/printsyslog.sh
+++
@@ -1,4 +0,0 @@
-tail -n 20 /var/log/syslog -F 
-echo -e "\n"
-ps -axj | grep -E 'vcsmd|VCF'
-echo -e "\n"
